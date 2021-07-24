5398e9cec73bda5350c884c5d994f70c95c550d4
fiorin <diana.zulcheva@gmail.com>
2021-07-24 21:05:54 +0200

    feat: create basic view for book creation page

--- whatwasthatbookcalled/books/views.py
+++ whatwasthatbookcalled/books/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 
 
 def create(req):
-    form = BookForm()
-    return render(req, "books/create.html", context={"form": form})
+    if req.method == "GET":
+        form = BookForm()
+        return render(req, "books/create.html", context={"form": form})
+    elif req.method == "POST":
+        form = BookForm(req.POST)
+        if form.is_valid():
+            print(req.POST)
+            form.save()
+        else:
+            print(req.POST)
+
+            return render(req, "books/create.html", context={"form": form})
