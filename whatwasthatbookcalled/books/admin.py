aff18bbf02352efe4d34cfdd06d874a5b291748b
fiorin <diana.zulcheva@gmail.com>
2021-07-24 21:04:43 +0200

    chore: register models

--- whatwasthatbookcalled/books/admin.py
+++ whatwasthatbookcalled/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from whatwasthatbookcalled.books.models import Book, BookGenre
 
 # Register your models here.
+
+admin.site.register(Book)
+
+admin.site.register(BookGenre)
