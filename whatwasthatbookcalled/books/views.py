c2b17acbc4f2862fbf93c2d504e990cf3f6e066e
fiorin <diana.zulcheva@gmail.com>
2021-08-02 00:23:37 +0200

    feat: add filled_fields_count field before saving instance

--- whatwasthatbookcalled/books/views.py
+++ whatwasthatbookcalled/books/views.py
@@ -1,5 +1,4 @@
-from whatwasthatbookcalled.books.foms import BookForm
-from django.shortcuts import render
+from django.db.models import QuerySet
 
 
 def create(req):
@@ -9,9 +8,20 @@ def create(req):
     elif req.method == "POST":
         form = BookForm(req.POST)
         if form.is_valid():
-            print(req.POST)
-            form.save()
+            book = form.save(commit=False)
+
+            filled_fields = [
+                x
+                for x in form.cleaned_data.values()
+                if x != ""
+                and x is not None
+                and not (isinstance(x, QuerySet) and len(x) == 0)
+            ]
+            book.filled_fields_count = len(filled_fields)
+            book.save()
+
+            return redirect("index")
         else:
             print(req.POST)
-
+            print("INVALID")
             return render(req, "books/create.html", context={"form": form})
