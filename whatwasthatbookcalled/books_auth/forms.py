942052524f58654eec7f6c49e8bdd3c305a8fbf1
fiorin <diana.zulcheva@gmail.com>
2021-08-13 14:14:04 +0200

    feat: style sign up and sign in form

--- whatwasthatbookcalled/books_auth/forms.py
+++ whatwasthatbookcalled/books_auth/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django import forms
 from whatwasthatbookcalled.books_auth.models import BookUser
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
@@ -6,10 +7,34 @@ UserModel = get_user_model()
 
 
 class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["password1"].help_text = None
+        self.fields["password1"].label = ""
+        self.fields["password1"].widget = forms.PasswordInput(
+            attrs={"placeholder": "Password"}
+        )
+
+        self.fields["password2"].help_text = None
+        self.fields["password2"].label = ""
+        self.fields["password2"].widget = forms.PasswordInput(
+            attrs={"placeholder": "Password confirmation"}
+        )
+
     class Meta:
 
         model = BookUser
         fields = ("username", "email")
+
+        help_texts = {"username": None}
+
+        widgets = {
+            "username": forms.TextInput(attrs={"placeholder": "Username"}),
+            "email": forms.TextInput(attrs={"placeholder": "Email"}),
+        }
+
+        labels = {"username": "", "email": "", "password1": "", "password2": ""}
 
 
 class LoginForm(AuthenticationForm):
--- whatwasthatbookcalled/books_auth/views.py
+++ whatwasthatbookcalled/books_auth/views.py
@@ -6,30 +6,28 @@ from django.shortcuts import redirect, render
 def sign_up(req):
     if req.method == "GET":
         form = RegisterForm()
-        return render(req, "auth/register.html", {"form": form})
+        return render(req, "auth/register-login.html", {"form": form})
 
     form = RegisterForm(req.POST)
-    print(form)
 
     if form.is_valid():
         form.save()
-        return redirect("/books")
+        return redirect("sign in")
     else:
-        return render(req, "auth/register.html", {"form": form})
+        return render(req, "auth/register-login.html", {"form": form})
 
 
 def sign_in(req):
     if req.method == "GET":
         form = LoginForm()
-        return render(req, "auth/register.html", {"form": form})
+        return render(req, "auth/register-login.html", {"form": form, "sign_in": True})
 
     form = LoginForm(req, req.POST)
-    print(form)
     if form.is_valid():
         login(req, form.user_cache)
         return redirect("/books")
     else:
-        return render(req, "auth/register.html", {"form": form})
+        return render(req, "auth/register-login.html", {"form": form, "sign_in": True})
 
 
 def sign_out(req):
