b40b85728b824219b1375d4fb323d8a9499327d9
fiorin <diana.zulcheva@gmail.com>
2021-08-20 21:40:55 +0200

    refactor: fix url element order

--- whatwasthatbookcalled/books/urls.py
+++ whatwasthatbookcalled/books/urls.py
@@ -12,11 +12,12 @@ from whatwasthatbookcalled.books.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("create/", create, name="create"),
-    path("details/<int:id>", details, name="details"),
+    path("<int:id>/details", details, name="details"),
     path(
         "solution-pick/<int:book_id>/<int:comment_id>",
         mark_comment_as_solution,
         name="solution pick",
     ),
+    path("<int:book_id>/edit", edit_book, name="edit"),
     path("<int:book_id>/delete", delete_book, name="delete"),
 ]
